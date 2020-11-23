7e3220313d39780cd10f0fe5de3e717b7e1c6fb7
kunalkumar007 <kunalkumar06022003@gmail.com>
2020-11-24 01:59:38 +0530

    drag feature added

--- app.js
+++ app.js
@@ -19,9 +19,60 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   createBoard();
 
-//Drag the Candies.
+  //Drag the Candies.
+  let colorBeingDragged,
+    colorBeingReplaced,
+    squareIdBeingDragged,
+    squareIdBeingReplaced;
 
+  squares.forEach((square) => square.addEventListener('dragstart', dragStart));
+  squares.forEach((square) => square.addEventListener('dragend', dragEnd));
+  squares.forEach((square) => square.addEventListener('dragover', dragOver));
+  squares.forEach((square) => square.addEventListener('dragenter', dragEnter));
+  squares.forEach((square) => square.addEventListener('dragleave', dragLeave));
+  squares.forEach((square) => square.addEventListener('drop', dragDrop));
 
+  function dragStart() {
+    colorBeingDragged = this.style.backgroundColor;
+    squareIdBeingDragged = parseInt(this.id);
+    console.log(
+      colorBeingDragged,
+      squareIdBeingDragged,
+      squareIdBeingReplaced,
+      'dragStart'
+    );
+  }
 
+  function dragOver(e) {
+    e.preventDefault();
+    console.log(this.id, 'dragOVer');
+  }
+
+  function dragEnter(e) {
+    e.preventDefault();
+    console.log(this.id, 'dragEnter');
+  }
+
+  function dragLeave() {
+    console.log(this.id, 'dragLeave');
+  }
+
+  function dragDrop() {
+    colorBeingReplaced = this.style.backgroundColor;
+    squareIdBeingReplaced = parseInt(this.id);
+    console.log(colorBeingReplaced, 'dragDrop');
+    this.style.backgroundColor = colorBeingDragged;
+    squares[squareIdBeingDragged].style.backgroundColor = colorBeingReplaced;
+  }
+  function dragEnd() {
+    console.log(this.id, 'dragEnd');
+    let validMoves = [
+      squareIdBeingDragged - 1,
+      squareIdBeingDragged - width,
+      squareIdBeingDragged + 1,
+      squareIdBeingDragged + width,
+    ];
+    let validMove = validMoves.includes(squareIdBeingReplaced);
+
+  }
 });
-
