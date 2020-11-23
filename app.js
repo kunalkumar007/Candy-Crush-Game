2938ba4f7f22d631711ed59fb7d0ac0279225565
kunalkumar007 <kunalkumar06022003@gmail.com>
2020-11-24 01:19:08 +0530

    create board

---
+++ app.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  const width = 8;
+  const squares = [];
+  const candyColors = ['red', 'yellow', 'orange', 'purple', 'green', 'blue'];
+
+  //create board
+  function createBoard() {
+    for (let i = 0; i < width * width; i++) {
+      const square = document.createElement('div');
+      square.setAttribute('draggable', true);
+      square.setAttribute('id', i);
+      let randomColor = Math.floor(Math.random() * candyColors.length);
+      square.style.backgroundColor = candyColors[randomColor];
+
+      grid.appendChild(square);
+      squares.push(square);
+    }
+  }
+  createBoard();
+
+//Drag the Candies.
+
+
+
+});
+
